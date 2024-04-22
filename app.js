646f35380b96eeb8f093ae5cee0130acd0d21319
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 10:33:39 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // write code here
+<script>
+        let browser = prompt('Enter your browser:');
+
+        if (browser === 'Edge') {
+        alert("You've got the Edge!");
+        } else if (browser === 'Chrome' || browser === 'Firefox' || browser === 'Safari' || browser === 'Opera') {
+        alert('Okay we support these browsers too');
+        } else {
+        alert('We hope that this page looks ok!');
+        }
+
+</script>
